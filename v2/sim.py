1e19dc4da37870373b1dc92fc0276589b0da3f2c
moisesoliv <moises.oliveira@ecomp.ufsm.br>
2019-05-24 17:09:09 -0300

    teste de escalonamento em fog heterogenea

--- v2/sim.py
+++ v2/sim.py
@@ -7,10 +7,12 @@ class FogDevice:
     CPU = 0     # 0 to 100%
     MEM = 0     # 0 to 100%
     NET = 0     # 0 to 100%
+    DISK = 0
 
     maxCPU = 100
     maxMEM = 100
     maxNET = 100
+    maxDISK = 100
 
     dropsCount = 0
     sucessCount = 0
@@ -18,22 +20,30 @@ class FogDevice:
     times = dict()
     CPU_history = dict()
     MEM_history = dict()
+    NET_history = dict()
+    DISK_history = dict()
     _printLock = th.Lock()
     _writeLock = th.Lock()
 
     def __init__(self, i):
-        self.cpuCount = 1
-        self.ramAmount = 1024
-        self.netCapacity = 10
         self.id = i
+
+        self.cpuCount = random.randint(1, 8)            # numero de cores
+        self.ramAmount = 2**random.randint(8, 13)       # Mb
+        self.netCapacity = random.randint(1, 100)*10    # Mbps
+        self.diskCapacity = random.randint(8, 500)*1000 # Mb vai de 5 até 500GB
+        self.CPU_freq = random.randint(10, 21)*100      # Mhz
+
         self.CPU_history[self.id] = list()
         self.MEM_history[self.id] = list()
+        self.NET_history[self.id] = list()
+        self.DISK_history[self.id] = list()
         self.times[self.id] = list()
 
     def doTask(self, task):
         self.tryCount += 1
         startTime = time.time()
-        time.sleep(random.randint(0, 10)/100)   # Setup time
+        # time.sleep(random.randint(0, 10)/100)   # Setup time
         if self.canReceive():
             # self.receiveTask(task)
             self.processTask(task)
@@ -46,19 +56,26 @@ class FogDevice:
 
     def processTask(self, task):
         with self._writeLock:
-            self.CPU += task['cpu']
-            self.MEM += task['mem']
+            self.CPU += task['cpu']/self.cpuCount
+            self.MEM += (task['mem']/self.ramAmount)*100
+            self.NET += (task['net']/self.netCapacity)*100
+            self.DISK += (task['disk']/self.diskCapacity)*100
 
-        time.sleep(task['time']/1000)
-        if self.CPU > self.CPU*0.9:
-            time.sleep(random.randint(0, 10)/1000)
+        time.sleep(task['time']/self.CPU_freq)
+
+        if self.CPU > self.maxCPU*0.9:
+            time.sleep(random.randint(0, 10)/self.CPU_freq)
 
         with self._writeLock:
-            self.CPU -= task['cpu']
-            self.MEM -= task['mem']
+            self.CPU -= task['cpu']/self.cpuCount
+            self.MEM -= (task['mem']/self.ramAmount)*100
+            self.NET -= (task['net']/self.netCapacity)*100
+            self.DISK -= (task['disk']/self.diskCapacity)*100
 
         self.CPU_history[self.id].append(self.CPU)
         self.MEM_history[self.id].append(self.MEM)
+        self.NET_history[self.id].append(self.NET)
+        self.DISK_history[self.id].append(self.DISK)
 
     def receiveTask(self, task):
         self.NET += task['net']
@@ -75,13 +92,42 @@ class FogDevice:
         return conditions
 
     def cpuAvg(self):
-        return sum(self.CPU_history[self.id])/len(self.CPU_history[self.id])
+        try:
+            avg = sum(self.CPU_history[self.id])/len(self.CPU_history[self.id])
+            return avg
+        except:
+            return 0
 
     def memAvg(self):
-        return sum(self.MEM_history[self.id])/len(self.MEM_history[self.id])
+        try:
+            avg = sum(self.MEM_history[self.id])/len(self.MEM_history[self.id])
+            return avg
+        except:
+            return 0
 
     def timeAvg(self):
-        return sum(self.times[self.id])/len(self.times[self.id])
+        try:
+            return sum(self.times[self.id])/len(self.times[self.id])
+        except:
+            return 0
+
+    def netAvg(self):
+        try:
+            return sum(self.NET_history[self.id])/len(self.NET_history[self.id])
+        except:
+            return 0
+
+    def diskAvg(self):
+        try:
+            return sum(self.DISK_history[self.id])/len(self.DISK_history[self.id])
+        except:
+            return 0
+
+    def dropRate(self):
+        try:
+            return self.dropsCount/self.tryCount
+        except:
+            return 0
 
 
 class Fog:
@@ -93,31 +139,56 @@ class Fog:
 
     def sendTask(self, task):
         with self._lock:
-            best_fog = self.getBestDevice()
+            best_fog = self.getBestDevice(task)
         self.devices[best_fog].doTask(task)
         # self.devices[best_fog].processTask(task)
 
-    def getBestDevice(self):
+    def getBestDevice(self, task):
         # return random.randint(0, self.n-1)
         # return self.bestCpuAverage()
         return self.bestDeviceAverage()
+        # return self.bestEderSchedule(task)
 
     def bestCpuAverage(self):
-        avg = [((d.CPU)) for d in self.devices]
+        avg = [d.CPU for d in self.devices]
         smaller = avg.index(min(avg))
         return self.devices[smaller].id
 
     def bestDeviceAverage(self):
-        # best = self.device[0]
         avg = [((d.CPU+d.MEM+d.NET)/3) for d in self.devices]
         smaller = avg.index(min(avg))
-        return self.devices[smaller].id
+        return smaller
+
+    def bestEderSchedule(self, task):
+        l = list()
+        for d in self.devices:
+            if (d.CPU > d.maxCPU * 0.9 and
+                d.MEM > d.maxMEM * 0.9 and
+                d.NET > d.maxNET * 0.9 and
+                d.DISK > d.maxDISK * 0.9):
+                r = 0
+            else:
+                try:
+                    r = ((d.cpuCount*d.CPU_freq)/(d.CPU/100) +
+                         (d.ramAmount*(d.MEM/100))/100 +
+                         (d.diskCapacity*(d.DISK/100))/1000)
+                except:
+                    r = ((d.cpuCount*d.CPU_freq) +
+                         (d.ramAmount/100) +
+                          d.diskCapacity/1000)
+            l.append(r)
+        best = l.index(max(l))
+        return best
 
     def printDevicesStat(self):
         for d in self.devices:
             print('\ndevice', d.id,
+                  '\nCores:', d.cpuCount, 'RAM:', d.ramAmount,
+                  '\nCPU freq:', d.CPU_freq,
+                  '\nNET:', d.netCapacity, 'Disk:', d.diskCapacity,
                   '\navgCPU: ', d.cpuAvg(), 'avg mem: ', d.memAvg(),
-                  '\nDropRate: ', (d.dropsCount/d.tryCount),
+                  '\navgNET: ', d.netAvg(), 'avg disk: ', d.diskAvg(),
+                  '\nDropRate: ', (d.dropRate()),
                   '\nTryCount:', d.tryCount,
                   '\nAvgTime:', d.timeAvg())
 
@@ -127,8 +198,9 @@ class Sensor:
     def __init__(self, i):
         self.DATA_LEN = 1024
         self.CPU_USE = 5        # %
-        self.FREQ = 1           # tempo entre requests em segundos
-        self.REQUESTS = 1       # num of requests
+        self.FREQ = random.randint(0, 100)/1000
+        # tempo entre requests em segundos
+        self.REQUESTS = 100       # num of requests
         self.ID = i
 
     def start(self, f):
@@ -137,18 +209,19 @@ class Sensor:
             time.sleep(self.FREQ)
 
     def requestTask(self, f):
-        task = {'cpu': 5,
-                'mem': 5,
-                'net': .5,
+        task = {'cpu': 5,       # 5% in one CPU core
+                'mem': 5,       # Mb
+                'disk': 30,     # Mb
+                'net': 1,       # Mb
                 'latency': 15,  # ms
-                'jitter': 5,    # ms
-                'time': 30}     # ms
+                'jitter': 5,    # Random de 0 até 5
+                'time': 30}     # em uma CPU de 1Ghz, demora 30ms
         f.sendTask(task)
 
 
 class Simulation:
     def __init__(self):
-        self.SENSORES = 1000
+        self.SENSORES = 10000
         self.FOGS = 10
 
     def sim(self):
